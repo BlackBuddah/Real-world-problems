605d9f4532c14bfd44c0df5bf28b2aa6a3e74e4a
BlackBuddah <149721470+BlackBuddah@users.noreply.github.com>
2023-11-02 16:00:57 -0500

    Add files via upload
    Roman Numerals
    This a program that prompts the user to enter a number within the range of 1 through 10. The program should display the Roman numeral version of that number. If the number is outside the range of 1 through 10, the program should display an error message. The following table shows the Roman numerals for the numbers 1 through 10:
              
              
              
              
                
                  
                  Number Roman Numeral1 I2 II3 III4 IV5 V6 VI7 VII8 VIII9 IX10 X

---
+++ Roman Numerals.py
@@ -0,0 +1,47 @@
+'''
+Author: Adesina Osibodu
+Date: 9/07/23
+Purpose: Week 3 Exercise
+'''
+
+##Number    Roman Numeral
+##1           i
+##2           ii
+##3          iii
+##4           iv
+##5           v
+##6           vi
+##7          vii
+##8          viii
+##9           ix
+##10           x
+
+
+
+#Ask user for Number
+number=int(input('Enter number between 1 to 10'   ))
+
+
+#Assign correct Roman numeral
+if number==1: 
+     print ('The Roman numeral version is I'   )
+elif number == 2:
+     print('The roman numeral version is ii'   )
+elif number == 3:
+     print('The roman numeral version is iii'  )
+elif number == 4:
+     print('The roman numeral version is iv'  )
+elif number == 5:
+     print('The roman numeral version is v' )
+elif number == 6:
+     print('The roman numeral version is vi'  )
+elif number == 7:
+     print('The roman numeral version is vii' )
+elif number == 8:
+     print('The roman numeral version is viii' )
+elif number == 9:
+     print ('The roman numeral version is ix' )
+elif number == 10:
+     print('The roman numeral version is x' )
+else:
+     print ('out of range')
