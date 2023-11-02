9be67df3e262586e7abd8e99a4554903b359b919
BlackBuddah <149721470+BlackBuddah@users.noreply.github.com>
2023-11-02 16:04:01 -0500

    Average Rainfall
    This program uses nested loops to collect data and calculate the average rainfall over a period of years. The program first asks for the number of years. The outer loop will iterate once for each year. The inner loop will iterate twelve times, once for each month. Each iteration of the inner loop will ask the user for the inches of rainfall for that month. After all iterations, the program will display the number of months, the total inches of rainfall, and the average rainfall per month for the entire period

---
+++ Rainfall.py
@@ -0,0 +1,46 @@
+'''
+Author: Adesina Osibodu
+Date: 9/14/23
+Purpose:Week 4 Exercise
+'''
+
+#Declaring Variables
+Years = 0
+Month = 0
+Average = 0
+Rainfall = 0.0
+
+#Ask user for the number of years
+
+years=int(input('Enter number of years:'))
+
+#Initial variable
+
+TotalInchesofRainfall=0
+TotalMonths=years * 12
+Averagerainfallpermonth=TotalInchesofRainfall/TotalMonths
+
+
+#Outerloop
+
+for year in range(1,years+1):
+    
+#Innerloop
+    
+    for month in range(1,13):
+        print('year',year,'month',month )
+        Rainfall=float(input('Total inches for month:'))
+
+       
+        TotalInchesofRainfall += Rainfall
+         
+
+#Average
+        
+Average=TotalInchesofRainfall/TotalMonths
+
+#output
+
+print(f'Number of Months: {TotalMonths}')
+print(f'Total inches of Rainfall: {TotalInchesofRainfall}')
+print(f'Average rainfall per month: {Average:.2f} inches') 
